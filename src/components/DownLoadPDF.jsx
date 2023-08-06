5cf44c4bd47cc5ea1d3668e4ccdf51f07f365d9e
yawarpwn <john.kazz@gmail.com>
2023-08-06 10:41:37 -0500

    blob pdf share window

--- src/components/DownLoadPDF.jsx
+++ src/components/DownLoadPDF.jsx
@@ -2,37 +2,54 @@ import { useEffect } from 'react';
 import { usePDF } from '@react-pdf/renderer';
 import PDFGenerator from './PDFGenerator';
 import DownloadIcon from '../icons/DownloadIcon';
-import WhatsappIcon from '../icons/WhatsappIcon'
+import ShareIcon from '../icons/ShareIcon'
 
 export default function DownloadPDF({ quotation }) {
-  console.log('DownnloadPDF - Lazy')
   const [instance, setInstance] = usePDF()
-  const pdfURL = instance.url || '';
 
   useEffect(() => {
     setInstance(<PDFGenerator quotation={quotation} />)
   }, [quotation])
 
-  const handleShareWhatsApp = () => {
-    const whatsappURL = `whatsapp://send?text=Compartiendo PDF de cotización&data=${encodeURIComponent(pdfURL)}`;
-    window.open(whatsappURL, '_blank');
+  const handleShare = async () => {
+    try {
+      // Generar el Blob desde el PDF generado
+      const pdfBlob = instance.blob;
+
+      // Comprobar si el navegador admite la API navigator.share
+      if (navigator.share) {
+        // Usar la API navigator.share para compartir el Blob del PDF
+        await navigator.share({
+          files: [new File([pdfBlob], `COT-2023-00${quotation.quo_number}.pdf`, { type: 'application/pdf' })],
+          title: `Cotización ${quotation.quo_number}`,
+          text: '¡Echa un vistazo a esta cotización!',
+        });
+      } else {
+        console.log('Tu navegador no admite la API navigator.share');
+      }
+    } catch (error) {
+      console.log('Error al compartir: ', error);
+    }
   };
 
   return (
     <div className='flex gap-x-1'>
-      <a
-        onClick={handleShareWhatsApp}
-        href={'#'}
-        download={`COT-2023-00${quotation.quo_number}.pdf`}
-      >
-        <WhatsappIcon />
-      </a>
-      <a
-        href={instance.url}
-        download={`COT-2023-00${quotation.quo_number}.pdf`}
-      >
-        <DownloadIcon />
-      </a>
+      {instance.loading ? '...' : (
+        <>
+          <button
+            onClick={handleShare}
+          >
+            <ShareIcon />
+          </button>
+          <a
+            href={instance.url}
+            download={`COT-2023-00${quotation.quo_number}.pdf`}
+          >
+            <DownloadIcon />
+          </a>
+
+        </>
+      )}
     </div>
   )
 }
